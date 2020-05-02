14e6bbe19e1aacfbe91fc9660e17453d239189be
Fate787 <64600851+Fate787@users.noreply.github.com>
2020-05-01 18:33:33 -0600

    Create doubleconsts.java

---
+++ java/com/ImYourFate/Auctions/misc/doubleconsts.java
@@ -0,0 +1,9 @@
+package play.dahp.us.auctions.misc;
+
+public class DoubleConsts
+{
+    public static final double THOUSAND = 1000.0;
+    public static final double MILLION = 1000000.0;
+    public static final double BILLION = 1.0E9;
+    public static final double TRILLION = 1.0E12;
+}
