78caa1913824399d730ce67507be6000c2d8f560
Fate787 <64600851+Fate787@users.noreply.github.com>
2020-05-01 20:42:57 -0600

    Create Authorizerexception.java

---
+++ java/com/ImYourFate/Intake/util/Auth/Authorizerexception.java
@@ -0,0 +1,23 @@
+package play.dahp.us.intake.util.auth;
+
+/**
+ * Thrown when not enough permissions are satisfied.
+ */
+public class AuthorizationException extends Exception {
+
+    public AuthorizationException() {
+    }
+
+    public AuthorizationException(String message) {
+        super(message);
+    }
+
+    public AuthorizationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AuthorizationException(Throwable cause) {
+        super(cause);
+    }
+
+}
